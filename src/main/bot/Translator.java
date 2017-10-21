ef94f8403255ca3b39fa05fbc9c8c1f18a1fd9b5
theleff <leff@leff.su>
2017-10-21 18:50:12 +0300

    Fix

--- src/main/bot/Translator.java
+++ src/main/bot/Translator.java
@@ -30,57 +30,62 @@ class Translator extends Processor {
     }
 
 
-    String YandexTranslate(String enteredText) throws IOException {
-
-        String textEscaped = enteredText.replace(" ", "%20");
-        String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170921T152134Z.93cdd37345a03a7f.3c0d40e83517a07da4922a1e2bf3902f6012822c&lang=" // key
-                + getLanguage(enteredText) + "&text=" + textEscaped;
-
-        URLConnection connection = null;
+    String YandexTranslate(String enteredText) {
 
         try {
-            connection = new URL(url).openConnection();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.printf("No text for translate");
-        }
+            String textEscaped = enteredText.replace(" ", "%20");
+            String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170921T152134Z.93cdd37345a03a7f.3c0d40e83517a07da4922a1e2bf3902f6012822c&lang=" // key
+                    + getLanguage(enteredText) + "&text=" + textEscaped;
 
-        InputStream response;
-
-        if (connection != null) {
-            response = connection.getInputStream();
-
-            final String[] json = new String[1];
-
-            final Runnable stuffToDo = new Thread(() -> json[0] = new Scanner(response).nextLine());
-
-            final ExecutorService executor = Executors.newSingleThreadExecutor();
-            final Future future = executor.submit(stuffToDo);
-            executor.shutdown();
+            URLConnection connection = null;
 
             try {
-                future.get(10, TimeUnit.SECONDS);
-            } catch (InterruptedException | ExecutionException ignored) {
-            } catch (TimeoutException e) {
-                return "Yandex Timeout";
-            }
-            if (!executor.isTerminated())
-                executor.shutdownNow();
-
-            int start = json[0].indexOf("[");
-            int end = json[0].indexOf("]");
-            String translated = json[0].substring(start + 2, end - 1);
-            System.out.println(json[0]);
-            if (enteredText.matches(translated)) {
-                History.add("ERROR - " + enteredText);
-                return "Слишком длинное выражение или ошибка";
+                connection = new URL(url).openConnection();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.printf("No text for translate");
             }
 
-            History.add(enteredText + " - " + translated);
-            return "Yandex: " + enteredText + " - " + translated;
+            InputStream response;
 
+            if (connection != null) {
+                response = connection.getInputStream();
+
+                final String[] json = new String[1];
+
+                final Runnable stuffToDo = new Thread(() -> json[0] = new Scanner(response).nextLine());
+
+                final ExecutorService executor = Executors.newSingleThreadExecutor();
+                final Future future = executor.submit(stuffToDo);
+                executor.shutdown();
+
+                try {
+                    future.get(10, TimeUnit.SECONDS);
+                } catch (InterruptedException | ExecutionException ignored) {
+                } catch (TimeoutException e) {
+                    return "Yandex Timeout";
+                }
+                if (!executor.isTerminated())
+                    executor.shutdownNow();
+
+                int start = json[0].indexOf("[");
+                int end = json[0].indexOf("]");
+                String translated = json[0].substring(start + 2, end - 1);
+                System.out.println(json[0]);
+                if (enteredText.matches(translated)) {
+                    History.add("ERROR - " + enteredText);
+                    return "Слишком длинное выражение или ошибка";
+                }
+
+                History.add(enteredText + " - " + translated);
+                return "Yandex: " + enteredText + " - " + translated;
+
+            }
+            return "Can't connect to Yandex";
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return "And what do you want me to do with it?";
         }
-        return "Can't connect to Yandex";
-
     }
 
     @Override
